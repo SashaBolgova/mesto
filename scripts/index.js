638ccd9580e54bea692c2a519c97dbca597ab1df
Sasha Bolgova <aleks.kostyleva@yandex.ru>
2022-11-14 10:46:27 +0300

    refactor: исправить ошибки для отправки на третюю проверку

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__toggle');
 const popupOpenButtonElement = document.querySelector('.profile__edit');
-const formElement = popupElement.querySelector('.popup__container')
+const formElement = popupElement.querySelector('.popup__form')
 const nameInput = popupElement.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_profession');
 const nameElement = document.querySelector('.profile__name');
@@ -13,6 +13,8 @@ const professionElement = document.querySelector('.profile__profession');
 
 const openPopup = function () {
     popupElement.classList.add('popup_opened');
+    nameInput.value = nameElement.textContent;
+    jobInput.value = professionElement.textContent;
 }
 
 const closePopup = function () {
